46651fef5eb4e06a94d92b8873ce9184cc2bc433
SeokHyeon-Eom <eom2244@naver.com>
2019-11-25 20:52:53 +0900

    수정

--- xycar/assn2/11_25.py
+++ xycar/assn2/11_25.py
@@ -110,70 +110,70 @@ class LineDetector:
 
 # autodrive.py
 #!/usr/bin/env python
+# best type
+# import rospy, time
 
-import rospy, time
-
-from linedetector import LineDetector
-from obstacledetector import ObstacleDetector
-from motordriver import MotorDriver
+# from linedetector import LineDetector
+# from obstacledetector import ObstacleDetector
+# from motordriver import MotorDriver
 
 
-class AutoDrive:
+# class AutoDrive:
 
-    def __init__(self):
-        rospy.init_node('xycar_driver')
-        self.line_detector = LineDetector('/usb_cam/image_raw')
-        self.obstacle_detector = ObstacleDetector('/ultrasonic')
-        self.driver = MotorDriver('/xycar_motor_msg')
+#     def __init__(self):
+#         rospy.init_node('xycar_driver')
+#         self.line_detector = LineDetector('/usb_cam/image_raw')
+#         self.obstacle_detector = ObstacleDetector('/ultrasonic')
+#         self.driver = MotorDriver('/xycar_motor_msg')
 
-    def trace(self):
-        obs_l, obs_m, obs_r = self.obstacle_detector.get_distance()
-        line_l, line_r = self.line_detector.detect_lines()
-        self.line_detector.show_images(line_l, line_r)
-        angle = self.steer(line_l, line_r)
-        speed = self.accelerate(angle, obs_l, obs_m, obs_r)
-        self.driver.drive(angle + 90, speed + 90)
+#     def trace(self):
+#         obs_l, obs_m, obs_r = self.obstacle_detector.get_distance()
+#         line_l, line_r = self.line_detector.detect_lines()
+#         self.line_detector.show_images(line_l, line_r)
+#         angle = self.steer(line_l, line_r)
+#         speed = self.accelerate(angle, obs_l, obs_m, obs_r)
+#         self.driver.drive(angle + 90, speed + 90)
 
-    def steer(self, left, right):
-        mid = (left + right) // 2
-        if mid < 280:
-            angle = -40
-        elif mid > 360:
-            angle = 40
-        else:
-            if 510 <= right <= 560:
-                angle = -20
-            elif 100 <= left <= 150:
-                angle = 20
-            else:
-                angle = 0
+#     def steer(self, left, right):
+#         mid = (left + right) // 2
+#         if mid < 280:
+#             angle = -40
+#         elif mid > 360:
+#             angle = 40
+#         else:
+#             if 510 <= right <= 560:
+#                 angle = -20
+#             elif 100 <= left <= 150:
+#                 angle = 20
+#             else:
+#                 angle = 0
         
-        print("reft",left,"mid",mid,"right", right,)
-        print("")
+#         print("reft",left,"mid",mid,"right", right,)
+#         print("")
         
-        return angle
+#         return angle
 
-    def accelerate(self, angle, left, mid, right):
-        if min(left, mid, right) < 50:
-            speed = 0
-        if angle < -20 or angle > 20:
-            speed = 20
-        else:
-            speed = 30
-        return speed
+#     def accelerate(self, angle, left, mid, right):
+#         if min(left, mid, right) < 50:
+#             speed = 0
+#         if angle < -20 or angle > 20:
+#             speed = 20
+#         else:
+#             speed = 30
+#         return speed
 
-    def exit(self):
-        print('finished')
+#     def exit(self):
+#         print('finished')
 
 
-if __name__ == '__main__':
-    car = AutoDrive()
-    time.sleep(3)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        car.trace()
-        rate.sleep()
-    rospy.on_shutdown(car.exit)
+# if __name__ == '__main__':
+#     car = AutoDrive()
+#     time.sleep(3)
+#     rate = rospy.Rate(10)
+#     while not rospy.is_shutdown():
+#         car.trace()
+#         rate.sleep()
+#     rospy.on_shutdown(car.exit)
 
 # test
 #!/usr/bin/env python
